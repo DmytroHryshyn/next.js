0ffff4bebdc85ed6041fbaa6f786485655896dde
dhryshyn <dev.dmytroh@gmail.com>
2023-07-10 15:53:20 +0300

    [Intuita] codemod "App Directory Boilerplate" (4 files changed, estimated: 5% of changes in the PR)

---
+++ examples/cms-wordpress/app/_page.tsx
@@ -0,0 +1,9 @@
+export default function RootPage({
+  params,
+  searchParams,
+}: {
+  params: { slug: string }
+  searchParams: { [key: string]: string | string[] | undefined }
+}) {
+  return null
+}
